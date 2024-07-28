02c58e63a12286d04f30c7bcaaad88889346e3d8
Zander Gall <alexanderdgall@gmail.com>
2024-07-28 12:20:32 -0500

    Switch from NASM and Heap to GAS and Stack

--- bfCompiler.c
+++ bfCompiler.c
@@ -2,22 +2,46 @@
 #include <string.h>
 #include <stdlib.h>
 
-const char* header = "section .data\n	memory dq 0\nsection .text\noutput:\n	mov rax, 1\n	mov rdx, 1\n	mov rsi, rbx\n	mov rdi, 1\n	syscall\n	ret\ninput:\n	mov rax, 0\n	mov rdx, 1\n	mov rsi, rbx\n	mov rdi, 0\n	syscall\n	ret\nglobal _start\n_start:\n	mov rax, 9\nmov rsi, 30000\nmov rdi, 0\nmov rdx, 3\nmov r8, -1\nmov r9, 0\nmov r10, 0x22\nsyscall\nmov qword[memory], rax\nmov rbx, qword[memory]\n";
-const char* footer = "	mov rax, 60\n	mov rdi, 0\n	syscall\n";
+const char* header = 
+	".text\n"
+	".global main\n"
+	"main:\n"
+	"	/* Save RBP and use to store RSP */\n"
+	"	pushq %rbp\n"
+	"	movq %rsp, %rbp\n"
+	"	/* Reset RAX */\n"
+	"	xor %rax, %rax\n"
+	"	/* Untouched registers that are used for output */\n"
+	"	movq $0, %rdi\n"
+	"	movq $1, %rdx\n"
+	"	/* Generate 30,000 cells */\n"
+	"	movq $30000, %rax\n"
+	"	GEN:\n"
+	"		dec %rsp\n"
+	"		movb $0, (%rsp)\n"
+	"		dec %rax\n"
+	"		jnz GEN\n"
+	"	/* Move Stack Pointer to center of stack space */\n"
+	"	addq $15000, %rsp\n"
+	"	/* ~~~~~~ Begin Brain Fuck ~~~~~~ */\n";
+
+const char* footer =
+	"	popq %rbp\n"
+	"	movl $0, %eax\n"
+	"	call exit\n";
 
 int stack[1024];
 int stackTop = 0;
 int main(int argc, char const *argv[]) {
 	if(argc < 2) {
 		printf("%s: Syntax: bfc [-g] input.bf\n", argv[0]);
-	}
+	}	
 
 	int debug = 0;
-	const char* filepath = argv[1];
-	if(strcmp(argv[1], "-g")==0) {
+	const char* filepath = argv[argc-1];
+	if(strcmp(argv[1], "-g")==0 || (argc > 2 && strcmp(argv[2], "-g")==0))
 		debug = 1;
-		filepath = argv[2];
-	}
+
 
 	int filenameLen;
 	for(filenameLen = strlen(filepath)-1; filenameLen >= 0 && filepath[filenameLen]!='.'; filenameLen--);
@@ -32,7 +56,7 @@ int main(int argc, char const *argv[]) {
 
 	FILE *file = fopen(filepath, "r");
 	FILE *asmFile = fopen(asmFilePath, "w+");
-
+	
 	fputs(header, asmFile);
 	int i = fgetc(file);
 	int rbxUpToDate = 0;
@@ -50,40 +74,48 @@ int main(int argc, char const *argv[]) {
 		switch(i) {
 		case '>':
 			if(number==1)
-				fprintf(asmFile, "inc rbx\n");
+				fprintf(asmFile, "decq %%rsp\n");
 			else
-				fprintf(asmFile, "add rbx, %d\n", number);
+				fprintf(asmFile, "subq $%d, %%rsp\n", number);
 			break;
 		case '<':
 			if(number==1)
-				fprintf(asmFile, "dec rbx\n");
+				fprintf(asmFile, "incq %%rsp\n");
 			else
-				fprintf(asmFile, "sub rbx, %d\n", number);
+				fprintf(asmFile, "addq $%d, %%rsp\n", number);
 			break;
 		case '-':
 			if(number==1)
-				fprintf(asmFile, "dec byte[rbx]\n");
+				fprintf(asmFile, "decb (%%rsp)\n");
 			else
-				fprintf(asmFile, "sub byte[rbx], %d\n", number);
+				fprintf(asmFile, "subb $%d, (%%rsp)\n", number);
 			break;
 		case '+':
 			if(number==1)
-				fprintf(asmFile, "inc byte[rbx]\n");
+				fprintf(asmFile, "incb (%%rsp)\n");
 			else
-				fprintf(asmFile, "add byte[rbx], %d\n", number);
+				fprintf(asmFile, "addb $%d, (%%rsp)\n", number);
 			break;
 		case '.':
-			fprintf(asmFile, "call output\n");
+			fprintf(asmFile, "movq %%rsp, %%rsi\n");
+			fprintf(asmFile, "movq -8(%%rsp), %%r8\n");
+			fprintf(asmFile, "call write\n");
+			fprintf(asmFile, "movq %%r8, -8(%%rsp)\n");
 			break;
 		case ',':
-			fprintf(asmFile, "call input\n");
+			fprintf(asmFile, "movq %%rsp, %%rsi\n");
+			fprintf(asmFile, "movq -8(%%rsp), %%r8\n");
+			fprintf(asmFile, "call read\n");
+			fprintf(asmFile, "movb (%%rsp), %%r9b\n");
+			fprintf(asmFile, "movq %%r8, -8(%%rsp)\n");
+			fprintf(asmFile, "movb %%r9b, (%%rsp)\n");
 			break;
 		case '[':
 			for(int n = 0; n < number; n++) {
 				if(stackTop==1024)
 					printf("%s error: Loop stack limit reached! (1024) Char: #%d\n", argv[0], c);
 				else {
-					fprintf(asmFile, "l%d:\ncmp byte[rbx], 0\nje e%d\n",label, label);
+					fprintf(asmFile, "L%d:\ncmpb $0, (%%rsp)\nje E%d\n", label, label);
 					stack[stackTop] = label;
 					stackTop++;
 					label++;
@@ -96,7 +128,7 @@ int main(int argc, char const *argv[]) {
 					printf("%s error: ']' present with no corresponding '['! Char: #%d\n", argv[0], c);
 				else {
 					stackTop--;
-					fprintf(asmFile, "cmp byte[rbx], 0\njne l%d\ne%d:\n", stack[stackTop], stack[stackTop]);
+					fprintf(asmFile, "jmp L%d\nE%d:\n", stack[stackTop], stack[stackTop]);
 				}
 			}
 			break;
@@ -109,12 +141,13 @@ int main(int argc, char const *argv[]) {
 		printf("%s error: %d '[' present with no corresponding ']'!", argv[0], stackTop);
 
 	fputs(footer, asmFile);
+	
 	fclose(file);
 	fclose(asmFile);
-	char *command = malloc(49 + 10*debug + 4*filenameLen+1);
-	sprintf(command, "yasm -f elf -m amd64%s '%s.s' -o '%s.o' && ld '%s.o' -o '%s'", debug==1 ? " -g dwarf2" : "", filename, filename, filename, filename);
-	system(command);
+	char* command = malloc(15 + 3*debug + 2*filenameLen+1);
+	sprintf(command, "gcc %s.s %s-m64 -o %s", filename, debug == 1 ? "-g " : "", filename);
+	int res = system(command);
 	printf("%s\n", command);
 
-	return 0;
+	return res;
 }
